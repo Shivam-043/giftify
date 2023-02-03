909cb29f79ab7ba8281045cbef84f3ef02f1543b
Shivam Varshney <12111043@nitkkr.ac.in>
2023-02-03 15:28:50 +0530

    changing backend database

--- README.md
+++
@@ -1,2 +0,0 @@
-# giftify
-Giftify
--- frontend/costumer_detail/detail.js
+++ frontend/costumer_detail/detail.js
@@ -14,36 +14,31 @@ function formaction() {
   var data = {
     colors: JSON.stringify(data.colors),
     addons: JSON.stringify(data.addons),
-    check_types:JSON.stringify(data.check_types),
-    occasion:JSON.stringify(data.occasion),
-    occ_text:JSON.stringify(data.occ_text),
-    text_screen:JSON.stringify(data.text_screen),
-    special_product:JSON.stringify(data.special_product),
+    check_types: JSON.stringify(data.check_types),
+    occasion: JSON.stringify(data.occasion),
+    occ_text: JSON.stringify(data.occ_text),
+    text_screen: JSON.stringify(data.text_screen),
+    special_product: JSON.stringify(data.special_product),
     product_id: product_id,
     total_price: Total_price,
     name: document.forms[0].name.value,
     email: document.forms[0].email.value,
     mobile_no: document.forms[0].mobile_no.value,
     address: document.forms[0].address.value,
-    Name_on_Gift: document.forms[0].Name_on_Gift.value, 
-    otp : document.forms[0].OTP_verify.value
-
+    Name_on_Gift: document.forms[0].Name_on_Gift.value,
   };
-  if (ValidateEmail(data.email) && ValidateMobile_no(data.mobile_no)) {
+  if (
+    ValidateEmail(data.email) &&
+    ValidateMobile_no(data.mobile_no) 
+  ) {
+    console.log("true otp");
     axios
       .post(Server2, data)
       .then((res) => {
-        console.log(res.data[0]);
-        if (res.data[0] == "success") {
-          if(data.otp == res.data[1])
-          {
-            alert("order successful");
-            window.location.href = "../index.html";
-          }
-          else{
-            alert("OTP is not same")
-          }
-
+        console.log(res.data);
+        if (res.data == "success") {
+          alert("order successful");
+          window.location.href = "../index.html";
         } else {
           alert("some error occured \ntry again");
         }
@@ -52,10 +47,8 @@ function formaction() {
         console.log(err);
       });
   }
-
   // console.log(data);
 }
-
 
 function ValidateEmail(inputText) {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -72,14 +65,64 @@ function ValidateEmail(inputText) {
   }
 }
 
-function ValidateMobile_no(inputText){
-    if (inputText.length==10) {
-        return true;
-      } else {
-        alert("mobile no should be 10 digit"); //The pop up alert for an invalid email address
-        //   document.form1.text1.focus();
-    
-        return false;
-      }
+function ValidateMobile_no(inputText) {
+  if (inputText.length == 10) {
+    return true;
+  } else {
+    alert("mobile no should be 10 digit"); //The pop up alert for an invalid email address
+    //   document.form1.text1.focus();
+
+    return false;
+  }
 }
 
+function ValidateOtp() {
+  var Server2 = Server + "/otp";
+  var data = JSON.parse(localStorage.getItem("data"));
+  var product_id = localStorage.getItem("product_id");
+  var Total_price = localStorage.getItem("Total_price");
+  //   document.getElementsByTagName("input")[0].value = JSON.stringify(data.colors);
+  //   document.getElementsByTagName("input")[1].value = JSON.stringify(data.addons);
+  //   document.getElementsByTagName("input")[2].value = product_id;
+  //   document.getElementsByTagName("input")[3].value = Total_price;
+  var data = {
+    colors: JSON.stringify(data.colors),
+    addons: JSON.stringify(data.addons),
+    check_types: JSON.stringify(data.check_types),
+    occasion: JSON.stringify(data.occasion),
+    occ_text: JSON.stringify(data.occ_text),
+    text_screen: JSON.stringify(data.text_screen),
+    special_product: JSON.stringify(data.special_product),
+    product_id: product_id,
+    total_price: Total_price,
+    name: document.forms[0].name.value,
+    email: document.forms[0].email.value,
+    mobile_no: document.forms[0].mobile_no.value,
+    address: document.forms[0].address.value,
+    Name_on_Gift: document.forms[0].Name_on_Gift.value,
+  };
+  var data2 = {
+    mob: data.mobile_no,
+  };
+  if (
+    ValidateEmail(data.email) &&
+    ValidateMobile_no(data.mobile_no) 
+  ) {
+    axios
+      .post(Server2, data2)
+      .then((res) => {
+        // console.log(res.data);
+
+        var userOtp = prompt("Enter OTP");
+        if (userOtp == res.data) {
+          alert("correct otp");
+          formaction();
+        } else {
+          alert("wrong OTP");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
